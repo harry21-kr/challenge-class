7f88d541e49ce6cca7b9df205b4974ba217754fc
박원빈 <harry21@kakao.com>
2024-05-28 15:59:17 +0900

    (add) FixedArray metsods

--- assignments-algorithm/fixed-array/FixedArray.js
+++ assignments-algorithm/fixed-array/FixedArray.js
@@ -42,4 +42,74 @@ class FixedArray {
     }
     return `[${str}]`;
   }
+
+  indexOf(searchElement) {
+    for (let i = 0; i < this.#index; i++) {
+      if (searchElement === this.#array[i]) {
+        return i;
+      }
+
+      return -1;
+    }
+  }
+
+  find(predicate) {
+    for (let i = 0; i < this.#index; i++) {
+      if (predicate(this.#array[i])) {
+        return this.#array[i];
+      }
+    }
+    return null;
+  }
+
+  findIndex(predicate) {
+    for (let i = 0; i < this.#index; i++) {
+      if (predicate(this.#array[i])) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  includes(searchElement) {
+    for (let i = 0; i < this.#index; i++) {
+      if (this.#array[i] === searchElement) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  forEach(callback) {
+    for (let i = 0; i < this.#index; i++) {
+      callback(this.#array[i]);
+    }
+  }
+
+  filter(predicate) {
+    const newArr = new FixedArray(this.#index);
+    for (let i = 0; i < this.#index; i++) {
+      if (predicate(this.#array[i])) {
+        newArr.push(this.#array[i]);
+      }
+    }
+    return newArr;
+  }
+
+  map(callback) {
+    const newArr = new FixedArray(this.#index);
+    for (let i = 0; i < this.#index; i++) {
+      newArr.push(callback(this.#array[i]));
+    }
+    return newArr;
+  }
+
+  reduce(callback, initValue) {
+    let newValue = initValue;
+    for (let i = 0; i < this.#index; i++) {
+      newValue = callback(newValue, this.#array[i]);
+    }
+    return newValue;
+  }
 }
